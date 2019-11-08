1282469d06b7b4f9aeac7ee3f8957dc24024b4d1
hungpv <tuhalang007@outlook.com.vn>
2019-11-08 16:35:33 +0700

    config error page && add config tag jsp

---
+++ web/src/main/java/com/nmcnpm/web/security/AuthorizationFilter.java
@@ -0,0 +1,34 @@
+package com.nmcnpm.web.security;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthorizationFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String url = request.getRequestURI();
+
+        if(url.startsWith("login") || url.startsWith("register")){
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else if(url.startsWith("admin")){
+
+        } else {
+
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
