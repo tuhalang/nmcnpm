3e02fed22ba4acaa80654d044a4872f632a1c005
Tran Van Hoang <hoang.tv173129@sis.hust.edu.vn>
2019-11-10 00:05:40 +0700

    add and complete class AccountDAO, CustomerDAO, CategoryDAO

--- web/src/main/java/com/nmcnpm/web/dao/AccountDAO.java
+++ web/src/main/java/com/nmcnpm/web/dao/AccountDAO.java
@@ -9,24 +9,60 @@ import com.nmcnpm.web.model.Account;
 
 public class AccountDAO extends DataBaseDaoImpl{
 	public void insert(Account account) {
-		String sql = "insert into account(account_id, username, password, status) value(?,?,?,?)";
+		String sql = "insert into account(account_id, username, password, status, created_at, last_modified_at) value(?,?,?,?,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP())";
 		insert(sql, account.getAccountID(), account.getUsername(), account.getPassword(), account.getStatus());
 	}
 	
 	public void update(Account account) {
-		String sql = "update user set username=?, password=?, status=? where account_id=?";
+		String sql = "update account set username=?, password=?, status=?, last_modified_at=CURRENT_TIMESTAMP() where account_id=?";
 		update(sql, account.getUsername(), account.getPassword(), account.getStatus(), account.getAccountID());
 		
 	}
 	
 	public void delete(Account account) {
-		String sql = "delete from account where username=? and password=? and maLop=?";
-		delete(sql, bd.getUser_id(), bd.getMaHP(), bd.getMaLop());
+		String sql = "delete from account where username=? and password=? and account_id=?";
+		delete(sql, account.getUsername(), account.getPassword(), account.getAccountID());
 	}
 	
-	public List<BangDiem> findAll() {
+	public List<Account> findAll() {
 		String sql = "select * from account";
-		List<BangDiem> mhs = query(sql, new AccountMapper());
-		return mhs;
+		List<Account> accounts = query(sql, new AccountMapper());
+		return accounts;
+	}
+
+	public Account findByUsernameAndPassword(String username, String password) {
+		String sql = "select * from account where username=? and password=?";
+		List<Account> accounts = query(sql,new AccountMapper(), username, password);
+		if(accounts.isEmpty())
+			return null;
+		return accounts.get(0);
+	}
+
+	public Account findById(long id) {
+		String sql = "select * from account where account_id=?";
+		List<Account> accounts = query(sql,new AccountMapper(), id);
+		if(accounts.isEmpty())
+			return null;
+		return accounts.get(0);
+	}
+
+	public List<Account> find(int start, int limit) {
+		String sql = "select * from account limit ?,?";
+		List<Account> accounts = query(sql,new AccountMapper(), start, limit);
+		return accounts;
+	}
+
+	public boolean isExist(String username) {
+		String sql = "select count(1) from account where username=? limit 1";
+		long count = count(sql, username);
+		if(count > 0)
+			return true;
+		return false;
+	}
+
+	public List<Account> findLikeUsername(String username) {
+		String sql = "select * from account where username like ?";
+		List<Account> accounts = query(sql, new AccountMapper(), "%"+username+"%");
+		return accounts;
 	}
 }
---
+++ web/src/main/java/com/nmcnpm/web/dao/CategoryDAO.java
@@ -0,0 +1,47 @@
+package com.nmcnpm.web.dao;
+
+import com.nmcnpm.database.dao.impl.DataBaseDaoImpl;
+import com.nmcnpm.web.mapprow.AccountMapper;
+import com.nmcnpm.web.mapprow.CategoryMapper;
+import com.nmcnpm.web.model.Account;
+import com.nmcnpm.web.model.Category;
+
+import java.util.List;
+
+public class CategoryDAO extends DataBaseDaoImpl {
+    public void insert(Category category) {
+        String sql = "insert into category(category_id, image, name, created_at, last_modified_at) value(?,?,?,CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP())";
+        insert(sql, category.getCategoryID(), category.getImage(), category.getName());
+    }
+
+    public void update(Category category) {
+        String sql = "update category set image=?, name=?, last_modified_at=CURRENT_TIMESTAMP() where category_id=?";
+        update(sql, category.getImage(), category.getName(), category.getCategoryID());
+
+    }
+
+    public void delete(Category category) {
+        String sql = "delete from category where category_id=? and image=? and name=?";
+        delete(sql, category.getCategoryID(), category.getImage(), category.getName());
+    }
+
+    public List<Category> findAll() {
+        String sql = "select * from category";
+        List<Category> category = query(sql, new CategoryMapper());
+        return category;
+    }
+
+    public Category findById(long id) {
+        String sql = "select * from category where category_id=?";
+        List<Category> category = query(sql,new CategoryMapper(), id);
+        if(category.isEmpty())
+            return null;
+        return category.get(0);
+    }
+
+    public List<Category> find(int start, int limit) {
+        String sql = "select * from category limit ?,?";
+        List<Category> category = query(sql,new CategoryMapper(), start, limit);
+        return category;
+    }
+}
