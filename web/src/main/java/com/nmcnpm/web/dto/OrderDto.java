d7a5b1a1190f0ea9ffedce3c0edffa63f4838e03
hungpv <tuhalang007@outlook.com.vn>
2019-11-29 20:45:19 +0700

    edit orderDto

--- web/src/main/java/com/nmcnpm/web/dto/OrderDto.java
+++ web/src/main/java/com/nmcnpm/web/dto/OrderDto.java
@@ -1,11 +1,9 @@
 package com.nmcnpm.web.dto;
 
-import com.nmcnpm.web.model.Product;
+import com.nmcnpm.web.model.OrderedProduct;
 import lombok.Data;
 
 @Data
-public class OrderDto {
-    private Product product;
-    private Long quantity;
+public class OrderDto extends ResponseDto<OrderedProduct>{
 
 }
